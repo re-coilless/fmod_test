6479e012b6c9b1d77d7727173f5b739c88a6a829
Recoilless Interactive <68288735+re-coilless@users.noreply.github.com>
2025-11-01 18:28:03 +0400

    test?

--- fmod_test/init.lua
+++ fmod_test/init.lua
@@ -1,5 +1,7 @@
 ModRegisterAudioEventMappings( "mods/fmod_tests/project/Build/GUIDs.txt" )
 
+--"mods/fmod_tests/project/Build/Desktop/fmod_test.bank"
+
 --test advanced control stuff
 --test general fmod features
 --check integration with vanilla systems
